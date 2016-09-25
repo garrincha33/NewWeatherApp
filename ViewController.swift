2af34324cbf49e8061e5cfe7af12ba120d584c8a
Richard Price <mrspondooli@gmail.com>
2016-09-25 17:26:37 +0100

    initial commit

---
+++ ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  NewWeatherApp
+//
+//  Created by Richard Price on 25/09/2016.
+//  Copyright © 2016 twisted echo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //test
+      
+    }
+
+  
+
+}
+
